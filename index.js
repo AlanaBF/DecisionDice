99f0967664b8e580f0f5312035f012a7b5bb4f02
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-04-26 13:43:27 +0100

    dice game refactored and styles

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  let randomDiceImage = "dice" + randomNumber1 + ".png";
+  let randomImageSource = "images/" + randomDiceImage;
+  let img1 = $("img")[0];
+  $(img1).attr("src", randomImageSource);
+
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  let randomDiceImage2 = "dice" + randomNumber2 + ".png";
+  let randomImageSource2 = "images/" + randomDiceImage2;
+  let img2 = $("img")[1];
+  $(img2).attr("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    $("h1").html("Player 1 wins");
+  } else if (randomNumber2 > randomNumber1) {
+    $("h1").html("Player 2 wins");
+  } else {
+    $("h1").html("Draw");
+  }
+ 
